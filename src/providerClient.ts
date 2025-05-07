c5f86866427bee70e07da37643116d1e0a25ab78
hamidra <hamid.alipour@gmail.com>
2025-05-07 17:56:12 -0400

    fix: fix the commads with empty env in config

--- src/providerClient.ts
+++ src/providerClient.ts
@@ -2,15 +2,30 @@ import { Client as McpClient } from "@modelcontextprotocol/sdk/client/index.js";
 import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js";
 import { SSEClientTransport } from "@modelcontextprotocol/sdk/client/sse.js";
 import { SERVER_NAME, SERVER_VERSION } from "./config";
-import { McpProvider } from "./store/schema";
+import { McpProvider, ProviderParameters } from "./store/schema";
 import { isStdioConfig, isSSEConfig } from "./store/schema";
 
 function getProviderClientTransport(mcpConfig: McpProvider) {
   if (isStdioConfig(mcpConfig)) {
-    return new StdioClientTransport(mcpConfig.providerParameters);
+    const { providerParameters } = mcpConfig;
+    // if the args is an empty array, delete it
+    if (providerParameters.args && providerParameters.args.length === 0) {
+      delete providerParameters.args;
+    }
+    // if the env is an empty object, delete it
+    if (
+      providerParameters.env &&
+      Object.keys(providerParameters.env).length === 0
+    ) {
+      delete providerParameters.env;
+    }
+    return new StdioClientTransport(providerParameters);
   }
   if (isSSEConfig(mcpConfig)) {
-    return new SSEClientTransport(new URL(mcpConfig.providerParameters.url));
+    const { providerParameters } = mcpConfig;
+    if (providerParameters.url) {
+      return new SSEClientTransport(new URL(providerParameters.url));
+    }
   }
   throw new Error(`Unsupported provider type: ${mcpConfig.type}`);
 }
